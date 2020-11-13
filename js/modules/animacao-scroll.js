4f947b89fb7898600a3ba6a893fb9f4b066e0222
JoneBulande <JoneBulande@gmail.com>
2020-11-13 21:08:59 +0200

    update

--- js/modules/animacao-scroll.js
+++ js/modules/animacao-scroll.js
@@ -3,18 +3,43 @@
 ///////////////////////////////////////////////
 export default function initAnimacaoScroll() {
 	
-	const downs = document.querySelectorAll('[data-anime="right"]');
+	const rights = document.querySelectorAll('[data-anime="right"]');
+	
+	const lefts = document.querySelectorAll('[data-anime="left"]');
+	
+	const ups = document.querySelectorAll('[data-anime="up"]');
+	
 	const windowMetade = window.innerHeight * 0.8;
 
 	function animationScroll(){	
 
-		downs.forEach((down)=>{
-			const top = down.getBoundingClientRect().top;
+		rights.forEach((right)=>{
+			const top = right.getBoundingClientRect().top;
 			const isVisible = (top - windowMetade) < 0;
 			if (isVisible) {
-				down.classList.add("ativo");
+			right.classList.add("ativo");
 			}else{
-				down.classList.remove("ativo");
+				right.classList.remove("ativo");
+			}
+		});
+		
+		lefts.forEach((left)=>{
+			const top = left.getBoundingClientRect().top;
+			const isVisible = (top - windowMetade) < 0;
+			if (isVisible) {
+				left.classList.add("ativo");
+			}else{
+				left.classList.remove("ativo");
+			}
+		});
+
+		ups.forEach((up)=>{
+			const top = up.getBoundingClientRect().top;
+			const isVisible = (top - windowMetade) < 0;
+			if (isVisible) {
+				up.classList.add("ativo");
+			}else{
+				up.classList.remove("ativo");
 			}
 		});
 	}
