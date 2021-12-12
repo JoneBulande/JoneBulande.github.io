a751252b2f59d63a48d31e011e12c05dbab35f32
JoneBulande <JoneBulande@gmail.com>
2021-12-13 00:14:51 +0200

    novo layout

--- js/script.js
+++
@@ -1,8 +0,0 @@
-import menuMobile from './modules/menumobile.js';
-import initScrollSuave from './modules/scrollsuave.js';
-import initAnimacaoScroll from './modules/animacao-scroll.js';
-
-
-menuMobile();
-initScrollSuave();
-initAnimacaoScroll();
